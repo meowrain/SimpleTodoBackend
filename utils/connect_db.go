97604abc659fafbbd0a827c96d42da39f075caba
meowrain <meowrain@126.com>
2024-03-21 00:29:19 +0800

    feat: 添加数据库操作日志系统

--- utils/connect_db.go
+++ utils/connect_db.go
@@ -4,12 +4,26 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
 	"todoBackend/app/config"
 )
 
 var db *gorm.DB
 
 func ConnectDB() *gorm.DB {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  false,       // Disable color
+		},
+	)
 	if db == nil {
 		//读取配置文件config.yaml
 		host := config.Cfg.DB.Host
@@ -19,7 +33,9 @@ func ConnectDB() *gorm.DB {
 		password := config.Cfg.DB.Password
 		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbName)
 		var err error
-		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+			Logger: newLogger,
+		})
 		if err != nil {
 			panic("failed to connect database")
 		}
