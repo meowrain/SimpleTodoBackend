52877e9a879a82491645f92990617d32f94074dd
meowrain <meowrain@126.com>
2024-03-21 00:43:07 +0800

    feat: 添加cors跨域功能

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 	"todoBackend/app/api/routes"
 	"todoBackend/app/config"
 	"todoBackend/utils"
@@ -11,7 +13,14 @@ func main() {
 	utils.ConnectDB()
 	utils.CreateTable()
 	router := gin.Default()
-
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	// 设置路由
 	routes.SetupUserRoutes(router)
 	routes.SetupTodoRoutes(router)
