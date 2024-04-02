76720237d345699379a4e61802629656026e8a3f
laydownooo <129923037+laydownooo@users.noreply.github.com>
2024-04-02 19:56:17 +0800

    Update user.go
    feat:新增user，Email，Gender,birthday，phonenumber四个属性以及更新以上数据的功能，更改bio的删改方式

--- app/models/user.go
+++ app/models/user.go
@@ -11,4 +11,8 @@ type User struct {
 	Avatar       string `gorm:"default:'http://127.0.0.1:8090/users/static/user.svg'" json:"avatar"`
 	Todos        []Todo `json:"todos"`
 	Bio          string `grom:"typr:text" json:"bio" validate:"max=250"`
+	Email        string `json:"email"  gorm:"type:varchar(254)"`
+	PhoneNumber  string `json:"phonenumber" validate:"max=20"`
+	Gender       string `json:"gender" gorm:"type:enum('男', '女');default:男"`
+	Birthday     string `json:"birthday" gorm:"type:varchar(25)"`
 }
